dca33ba3eb4210270e915fd37de8e737f2a669f2
DiegoV22 <diego.vega.aguilera@udla.edu.ec>
2024-05-02 07:37:38 -0500

    Primer

--- Models/Estudiante.cs
+++ Models/Estudiante.cs
@@ -1,7 +1,20 @@
-﻿namespace EVega.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EVega.Models
 {
     public class Estudiante
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public decimal Nota { get; set; }
+        public Boolean Estado { get; set; }
+        public DateTime Fecha { get; set; }
+
+        
+
+
 
     }
 }
